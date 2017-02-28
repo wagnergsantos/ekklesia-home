c27047917b44a27c294f21d382d9a56e3b79c32b
wagnergsantos <wagner.gsantos@gmail.com>
2017-02-28 19:36:25 -0300

    inicio do projeto

---
+++ src/main/java/br/com/geracaoelias/ekklesia_home/EkklesiaHomeApplication.java
@@ -0,0 +1,12 @@
+package br.com.geracaoelias.ekklesia_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkklesiaHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkklesiaHomeApplication.class, args);
+	}
+}
