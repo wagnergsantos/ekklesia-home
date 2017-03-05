cd86dbca896f4ab17e00728b1a9107cb1a909e14
wagnergsantos <wagner.gsantos@gmail.com>
2017-03-04 21:24:59 -0300

    scripts para criação do banco de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,52 @@
+insert into cargo (id,descricao) values (1, 'Diácono');
+insert into cargo (id,descricao) values (2, 'Evangelista');
+insert into cargo (id,descricao) values (3, 'Missionário');
+insert into cargo (id,descricao) values (4, 'Pastor');
+
+
+insert into escolaridade (id,descricao) values (1, 'Ensino Fundamental');
+insert into escolaridade (id,descricao) values (2, 'Ensino Médio');
+insert into escolaridade (id,descricao) values (3, 'Ensino Superior');
+insert into escolaridade (id,descricao) values (4, 'Especialização');
+insert into escolaridade (id,descricao) values (5, 'Mestrado');
+insert into escolaridade (id,descricao) values (6, 'Doutorado');
+
+insert into estado_civil (id,descricao) values (1, 'Casado');
+insert into estado_civil (id,descricao) values (2, 'Divorciado');
+insert into estado_civil (id,descricao) values (3, 'Solteiro');
+insert into estado_civil (id,descricao) values (4, 'Viúvo');
+
+
+insert into situacao_membro (id,descricao) values (1, 'Ativo');
+insert into situacao_membro (id,descricao) values (2, 'Afastado');
+insert into situacao_membro (id,descricao) values (3, 'Transferido');)
+
+
+
+insert into estados (sigla,nome) values ('AC','Acre');  
+insert into estados (sigla,nome) values ('AL','Alagoas');  
+insert into estados (sigla,nome) values ('AM','Amazonas');
+insert into estados (sigla,nome) values ('AP','Amapá');
+insert into estados (sigla,nome) values ('BA','Bahia');
+insert into estados (sigla,nome) values ('CE','Ceará');
+insert into estados (sigla,nome) values ('DF','Distrito Federal');
+insert into estados (sigla,nome) values ('ES','Espírito Santo');
+insert into estados (sigla,nome) values ('GO','Goiás');
+insert into estados (sigla,nome) values ('MA','Maranhão');
+insert into estados (sigla,nome) values ('MG','Minas Gerais');
+insert into estados (sigla,nome) values ('MS','Mato Grosso do Sul');
+insert into estados (sigla,nome) values ('MT','Mato Grosso');
+insert into estados (sigla,nome) values ('PA','Pará');
+insert into estados (sigla,nome) values ('PB','Paraíba');
+insert into estados (sigla,nome) values ('PE','Pernambuco');
+insert into estados (sigla,nome) values ('PI','Piauí');
+insert into estados (sigla,nome) values ('PR','Paraná');
+insert into estados (sigla,nome) values ('RJ','Rio de Janeiro');
+insert into estados (sigla,nome) values ('RN','Rio Grande do Norte');
+insert into estados (sigla,nome) values ('RO','Rondônia');
+insert into estados (sigla,nome) values ('RR','Roraima');
+insert into estados (sigla,nome) values ('RS','Rio Grande do Sul');
+insert into estados (sigla,nome) values ('SC','Santa Catarina');
+insert into estados (sigla,nome) values ('SE','Sergipe');
+insert into estados (sigla,nome) values ('SP','São Paulo');
+insert into estados (sigla,nome) values ('TO','Tocantins');
